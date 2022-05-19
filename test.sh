0d80927ce256bf8d3cab1a563f3b9d5bc4202e3a
Porfirio Chávez <hola@porfi.dev>
2022-05-19 14:02:28 -0500

    add scripts

---
+++ test.sh
@@ -0,0 +1,16 @@
+git filter-branch --env-filter '
+OLD_EMAIL="pchavez_ext@amco.mx"
+CORRECT_NAME="porfidev"
+CORRECT_EMAIL="elporfirio@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
